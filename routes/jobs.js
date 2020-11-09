ee7d41c105ad5b32f260052c3081e1f031179e79
Splinter1080 <61391824+Splinter1080@users.noreply.github.com>
2020-11-09 12:38:52 +0530

    Final changes

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const {isLoggedIn , isEmployer} = require('../middleware');
+const { isLoggedIn } = require('../middleware');
 const jobs = require('../controllers/jobs');
 
 
-router.route('/employer/jobDelete/:jobid/:uid')  
-.post(isLoggedIn ,   jobs.delJobs)
-router.route('/employer/jobEdit')  
-.get(isLoggedIn , isEmployer ,  jobs.editJobs)
+router.route('/employer/jobDelete/:jobid/:uid')
+    .post(isLoggedIn, jobs.delJobs)
+router.route('/employer/jobEdit')
+    .get(isLoggedIn, jobs.editJobs)
 router.route('/employer')
-    .get(isLoggedIn  ,   jobs.renderDetails)
-router.route('/employer/:id')  
-.post(isLoggedIn , isEmployer, jobs.registerJobDetails)
-router.route('/employer/edit/:id/:jobid')  
-.post(isLoggedIn , isEmployer, jobs.editJobDetails)
-router.route('/employer/:id')  
-.get(isLoggedIn , isEmployer,  jobs.listJobs)
+    .get(isLoggedIn, jobs.renderDetails)
+router.route('/employer/:id')
+    .post(isLoggedIn, jobs.registerJobDetails)
+router.route('/employer/edit/:id/:jobid')
+    .post(isLoggedIn, jobs.editJobDetails)
+router.route('/employer/:id')
+    .get(isLoggedIn, jobs.listJobs)
 
 
 
